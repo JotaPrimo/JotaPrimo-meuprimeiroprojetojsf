a3269de6b978286946d008e57ad75db09cbc3afe
JotaPrimo <gestald118@gmail.com>
2024-02-20 08:57:12 -0400

    Processando imagem e gravando no banco de dados

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,6 +1,8 @@
 package br.com.cursojsf;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -246,5 +248,26 @@ public class PessoaBean implements Serializable {
 		}			
 	}	
 	
-	private byte[] getByte(In)
+	private byte[] getByte(InputStream is) throws IOException {
+		int len;
+		int size = 1024;
+		byte[] buf = null;
+		
+		if(is instanceof ByteArrayInputStream) {
+			size = is.available();
+			buf = new byte[size];
+			len = is.read(buf, 0, size);
+		} else {
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			buf = new byte[size];
+			
+			while((len = is.read(buf, 0, size)) != -1) {
+				bos.write(buf, 0, size);
+			}
+			
+			buf = bos.toByteArray();
+		}		
+		
+		return buf;
+	}
 }
